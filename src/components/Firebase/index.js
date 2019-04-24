b1f6e12a2eac4a300e1747a946f1f898501324a3
Glitch (hello-express) <none>
2019-04-24 20:50:08 +0000

    🍲⛲️ Checkpoint ./src/index.js:614723/203 ./src/components/Firebase/index.js:614723/1252

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+}
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(config)
+    firebase.firestore().settings({ timestampsInSnapshots: true })
+    
+    this.firestore = firebase.firestore()
+    this.auth = firebase.auth()
+    
+    this.googleProvider = new firebase.auth.GoogleAuthProvider()
+  }
+}
+
+export default Firebase
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+
 import { collectionData } from 'rxfire/firestore';
 
-const app = firebase.initializeApp(
-  { projectId: 'ssj-document-manager' }
-);
-app.firestore().settings({ timestampsInSnapshots: true });
 
 class App extends React.Component {
   
