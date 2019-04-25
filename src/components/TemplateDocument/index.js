aae33b93bdb3aa1e04ad10316834de00878627e2
Glitch (hello-express) <none>
2019-04-25 18:39:49 +0000

    🕷🎸 Checkpoint ./src/components/TemplateDocument/index.js:614723/1081

--- src/components/TemplateDocument/index.js
+++ src/components/TemplateDocument/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { collectionData } from 'rxfire/firestore'
 
 import { withAuthorization } from '../Session'
 
@@ -10,6 +11,33 @@ const TemplatePageBase = () => (
     <p>The Admin Page is only accessible by the admins.</p>
   </div>
 );
+
+class TemplatePageBase extends React.Component {
+  constructor() {
+    super();
+    this.state = { templates: [] };
+  }
+  
+  componentDidMount() {
+    const templatesRef = app.firestore().collection('template_documents')
+    collectionData(templatesRef).subscribe(templates => {
+      this.setState({ templates })
+    });
+  }
+  
+  render() {
+    const lis = this.state.templates.map(c => {
+      return <li key={c.id}>{c.name} - {c.temperature}</li> 
+    });                               
+    return (
+      <div>
+      <h1>Template Document Page</h1>
+      <p>The Admin Page is only accessible by the admins.</p>
+        <ul>{lis}</ul>  
+      </div>
+    );
+  }
+}
 
 const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN]
 
