fe0b5eb09ff415ba86dc35f0456c14f41d59e2fc
Glitch (hello-express) <none>
2019-05-01 21:36:35 +0000

    ⛵️👩 Checkpoint ./src/components/TemplateDocument/create.js:614723/2006

--- src/components/TemplateDocument/create.js
+++ src/components/TemplateDocument/create.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Select from 'react-select';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { collectionData } from 'rxfire/firestore'
 
 import { withAuthorization, isAdmin } from '../Session'
@@ -12,70 +12,78 @@ import { STATES_AS_OPTIONS } from '../../constants/states'
 import { STAGES_AS_OPTIONS } from '../../constants/stages'
 
 const TemplateDocumentSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
+  document_name: Yup.string()
+    .min(1, 'Too Short!')
     .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
+  document_url: Yup.string()
+    .url('Invalid url')
     .required('Required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
-});
+  stage: Yup.string()
+    .ensure(),
+  all_states: Yup.boolean(),
+  states: Yup.array()
+    .of(Yup.string()
+        .ensure())
+})
 
 class CreateForm extends React.Component {
   constructor(props) {
     super(props)
+    
+    this.newTemplate = {
+      document_name: '',
+      document_url: '',
+      stage: '',
+      all_states: false,
+      states: []
+    }
   }
   
   render() {
     return (
       <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-        }}
-        validationSchema={SignupSchema}
+        initialValues={this.newTemplate}
+        validationSchema={TemplateDocumentSchema}
         onSubmit={values => {
           // same shape as initial values
           console.log(values);
         }}
       >
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Document Name:
-            <input type="text" defaultValue={this.state.newTemplate.document_name} />
-          </label>
-          <label>
-            Document URL:
-            <input type="text" value={this.state.newTemplate.document_url} />
-          </label>
-          <label>
-            Startup Journey Stage:
-            <Select
-              defaultValue={this.state.newTemplate.stage}
-              options={STAGES_AS_OPTIONS} />
-          </label>
-          <label>
-            Valid for All States?
-            <input
-              name="all_states"
-              type="checkbox"
-              defaultChecked={this.state.newTemplate.all_states} />
-          </label>
-          <label >
-            State
-            <Select
-              isDisabled={this.state.newTemplate.all_states}
-              defaultValue={this.state.newTemplate.states}
-              options={STATES_AS_OPTIONS}
-              isMulti={true} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        {({ errors, touched }) => (
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <label>
+              Document Name:
+              <input type="text" defaultValue={this.state.newTemplate.document_name} />
+            </label>
+            <label>
+              Document URL:
+              <input type="text" value={this.state.newTemplate.document_url} />
+            </label>
+            <label>
+              Startup Journey Stage:
+              <Select
+                defaultValue={this.state.newTemplate.stage}
+                options={STAGES_AS_OPTIONS} />
+            </label>
+            <label>
+              Valid for All States?
+              <input
+                name="all_states"
+                type="checkbox"
+                defaultChecked={this.state.newTemplate.all_states} />
+            </label>
+            <label >
+              State
+              <Select
+                isDisabled={this.state.newTemplate.all_states}
+                defaultValue={this.state.newTemplate.states}
+                options={STATES_AS_OPTIONS}
+                isMulti={true} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        )}
       </Formik>
     )
   }
