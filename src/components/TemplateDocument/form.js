3bbf1697dc4ce8ac89454c82dd65dcc6ac5dcc08
Glitch (hello-express) <none>
2019-05-02 21:32:39 +0000

    🐿🍄 Checkpoint ./src/components/TemplateDocument/create.js:614723/3321 ./src/components/TemplateDocument/form.js:614723/5447

--- src/components/TemplateDocument/form.js
+++ src/components/TemplateDocument/form.js
@@ -1,80 +1,104 @@
 import React from 'react'
 import Select from 'react-select';
-import { collectionData } from 'rxfire/firestore'
+import PropTypes from 'prop-types'
 
-import { withAuthorization, isAdmin } from '../Session'
-import { withFirebase } from '../Firebase'
-
-import * as ROLES from '../../constants/roles'
-import * as ROUTES from '../../constants/routes'
 import { STATES_AS_OPTIONS } from '../../constants/states'
 import { STAGES_AS_OPTIONS } from '../../constants/stages'
 
 class TemplateForm extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      error: null,
-      newTemplate: {
-        document_name: '',
-        document_url: '',
-        stage: '',
-        all_states: false,
-        states: []
-      }
-    }
-  }
-  
-  handleSubmit() {
-    return this.props.firebase.template_document().set(
-      this.state.template
-    )
-    .then(() => {
-      this.setState({ error: null })
-      this.props.history.push(ROUTES.ADMIN)
-    })
-  }
-  
   render() {
     return (
-      <div>
-        <h3>Create Template Document</h3>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Document Name:
-            <input type="text" defaultValue={this.state.newTemplate.document_name} />
-          </label>
-          <label>
-            Document URL:
-            <input type="text" value={this.state.newTemplate.document_url} />
-          </label>
-          <label>
-            Startup Journey Stage:
-            <Select
-              defaultValue={this.state.newTemplate.stage}
-              options={STAGES_AS_OPTIONS} />
-          </label>
-          <label>
-            Valid for All States?
-            <input
-              name="all_states"
-              type="checkbox"
-              defaultChecked={this.state.newTemplate.all_states} />
-          </label>
-          <label >
-            State
-            <Select
-              isDisabled={this.state.newTemplate.all_states}
-              defaultValue={this.state.newTemplate.states}
-              options={STATES_AS_OPTIONS}
-              isMulti={true} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <Formik
+        initialValues={this.newTemplate}
+        validationSchema={TemplateDocumentSchema}
+        onSubmit={(values, actions) => {
+          actions.setSubmitting(false)
+          
+          this.props.firebase.template_document().add(
+            values
+          )
+          .then(() => {
+            actions.setErrors(null)
+            actions.setSubmitting(false)
+            
+            this.props.history.push(ROUTES.ADMIN)
+          })
+         .catch(error => {
+            actions.setErrors(error)
+            actions.setSubmitting(false)
+          })
+        }}
+      >
+        {({ errors, touched, handleChange, setFieldTouched, setFieldValue, isSubmitting, isValid, values }) => (
+          <Form>
+            <label htmlFor="td_document_name">
+              Document Name:
+              <Field id="td_document_name" type="text" name="document_name" />
+              <ErrorMessage name="document_name" className="error" component="div" />
+            </label>
+            <label htmlFor="td_document_url">
+              Document URL:
+              <Field id="td_document_url" type="url" name="document_url" />
+              <ErrorMessage name="document_url" className="error" component="div" />
+            </label>
+            <label htmlFor="td_stage">
+              Startup Journey Stage:
+              <Field
+                name="stage"
+                component={({field}) =>
+                  <Select
+                    id="td_stage"
+                    isMulti={false}
+                    isClearable={true}
+                    value={STAGES_AS_OPTIONS ? STAGES_AS_OPTIONS.find(option => option.value === field.value) : ''}
+                    onChange={(option) => setFieldValue(field.name, (option === null ? '' : option.value) )}
+                    onBlur={field.onBlur}
+                    options={STAGES_AS_OPTIONS} />
+                }
+              />
+              <ErrorMessage name="stage" className="error" component="div" />
+            </label>
+            <label htmlFor="td_all_states">
+              Valid for All States?
+              <Field
+                id="td_all_states"
+                name="all_states"
+                type="checkbox"
+                checked={values.all_states} />
+              <ErrorMessage name="all_states" className="error" component="div" />
+            </label>
+            <label htmlFor="td_states">
+              State
+              <Field
+                name="states"
+                component={({field}) =>
+                  <Select
+                    id="td_states"
+                    isClearable={true}
+                    isDisabled={values.all_states}
+                    isMulti={true}
+                    value={STATES_AS_OPTIONS ? STATES_AS_OPTIONS.filter(option => field.value.includes(option.value)) : ''}
+                    onChange={(option) => setFieldValue(field.name, (option === null ? [] : option.map((o) => o.value)) )}
+                    onBlur={field.onBlur}
+                    options={STATES_AS_OPTIONS} />
+                }
+              />
+              <ErrorMessage name="states" className="error" component="div" />
+            </label>
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     )
   }
 }
+
+TemplateForm.propTypes = {
+  mode: PropTypes.oneOf(['create', 'edit']).isRequired,
+  onSuccess: PropType.func.isRequired
+  onFailure: PropType.func
+};
 
 export default TemplateForm
